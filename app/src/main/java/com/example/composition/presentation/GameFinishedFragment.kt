b035e2a11fb29ed036858f89ad12e941e155a871
Kuzmin_office <kuzmin2706@yandex.ru>
2022-05-18 07:46:01 +0400

    fragments_navigation_fragment_manager

--- app/src/main/java/com/example/composition/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/example/composition/presentation/GameFinishedFragment.kt
@@ -22,7 +22,6 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class GameFinishedFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var gameResult: GameResult? = null
 
     private var _binding: FragmentGameFinishedBinding? = null
@@ -80,7 +79,6 @@ class GameFinishedFragment : Fragment() {
          * @param param2 Parameter 2.
          * @return A new instance of fragment GameFinishedFragment.
          */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(gameResult: GameResult) =
             GameFinishedFragment().apply {
